2f558b2baf31bc0de4d3e934bb184f934e1b8f3e
m-kaltman <112902605+m-kaltman@users.noreply.github.com>
2023-07-17 14:22:37 -0700

    added RH_T Test harness

---
+++ RH_T_Replacement_Board/RH_T_HVT_V1/RH_T_Test_Harness/RH_T_Test_Harness.ino
@@ -0,0 +1,18 @@
+/*
+Greener GreenHouses RH/T Test Harness
+authors: mkaltman@ucsc.edu , tikraeme@ucsc.edu
+Date Created : 7/17/2023  2:19pm
+Last Edited : 7/17/2023   2:20pm
+*/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
