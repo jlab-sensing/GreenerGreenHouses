60b2e8fec2472883fc8b50195cfcfd4c5a4af5a9
Tim Kraemer <tim022402@gmail.com>
2023-07-24 22:56:45 -0700

    edits to voltages

--- Sensor Power Comparison/Temp_Power_Compare.m
+++ Sensor Power Comparison/Temp_Power_Compare.m
@@ -7,19 +7,21 @@ samples = linspace(1, 60, 60)
 
 %hdc2021 current calc at each sample rate
 hdc2021_curr = ((60-samples)*0.05+samples*650)/60
-hdc2021_pow = hdc2021_curr * 3
+%%1.8 V with this current consumption composition
+hdc2021_pow = hdc2021_curr * 1.8
 plot(samples, hdc2021_pow)
 hold on
 
 %htu21d current calc at each sample rate
-htu21d_curr = ((60-samples)*0.02+samples*500)/60
+htu21d_curr = ((60-samples)*0.02+samples*450)/60
 htu21d_pow = htu21d_curr * 3
 plot(samples, htu21d_pow)
 hold on
 
 %ens210 current calc at each sample rate
 ens210_curr = ((60-samples)*0.04+samples*58)/60
-ens210_pow = ens210_curr * 3
+%1.8 V with this current consumption composition 
+ens210_pow = ens210_curr * 1.8
 plot(samples, ens210_pow)
 
 
