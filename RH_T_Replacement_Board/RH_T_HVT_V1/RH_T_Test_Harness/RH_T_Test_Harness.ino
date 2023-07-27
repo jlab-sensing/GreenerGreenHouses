31b877e5fc4a501cf0c2226fb85c417c733b7c72
m-kaltman <112902605+m-kaltman@users.noreply.github.com>
2023-07-27 11:01:29 -0700

    initial test code complete. need to add softwire functionality to poll both I2C sensors

--- RH_T_Replacement_Board/RH_T_HVT_V1/RH_T_Test_Harness/RH_T_Test_Harness.ino
+++ RH_T_Replacement_Board/RH_T_HVT_V1/RH_T_Test_Harness/RH_T_Test_Harness.ino
@@ -2,15 +2,15 @@
 Greener GreenHouses RH/T Test Harness
 authors: mkaltman@ucsc.edu , tikraeme@ucsc.edu
 Date Created : 7/17/2023  2:19pm
-Last Edited : 7/19/2023   11:36pm
+Last Edited : 7/27/2023   11:00am
 */
 
 #include <HDC2080.h>
 #include <SD.h>
 #include <SPI.h>
 
+
 #define ADDR 0x40
-#define CS 10
 
 #define ARGUS_GH_Humidity "ARGH_H.txt"
 #define ARGUS_GH_Temp "ARGH_T.txt"
@@ -33,7 +33,6 @@ float temperature = 0, humidity = 0;
 
 void setup() {
   Serial.begin(9600);
-  while(!Serial);
 
   sensor.begin();
 
@@ -43,45 +42,49 @@ void setup() {
   sensor.setRate(ONE_HZ);
   sensor.setTempRes(FOURTEEN_BIT);
   sensor.setHumidRes(FOURTEEN_BIT);
-  pinMode(CS,OUTPUT);
+
   sensor.triggerMeasurement();
+
+ Serial.print("Initializing SD card...");
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+
+  //reading and storing GGH temp value
+  GGH_T = SD.open(GGH_GRO_Temp,FILE_WRITE);
+  
+// //reading and storing GGH humidity value
+  GGH_H = SD.open(GGH_GRO_Humidity,FILE_WRITE);
 }
+
+
 
 void loop() {
   
-  //making sure that the sd reader is initialized correctly
-  if(!SD.begin(CS)){
-    Serial.println("Initialization Failed...");
-    return;
-  }
-  Serial.println("SD Initialization Complete");
 
 //easy #ifdef to switch between which sensor is being used, ARGUS or GGH
 
-  #ifdef ARG
-  //reading and storing ARGUS humidity value
-  ARG_H = SD.open(ARGUS_GRO_Humidity,FILE_WRITE);
-  ARG_H.println(" %d ",sensor.readHumidity()); 
-  ARG_H.close();
+  float x,y;
+  x = sensor.readHumidity();
+  y = sensor.readTemp();
 
-//reading and storing ARGUS temperature value
-  ARG_T = SD.open(ARGUS_GRO_Temp,FILE_WRITE);
-  ARG_T.println(" %d ",sensor.readTemp());
-  ARG_T.close();
-  #endif
+  Serial.print("Temperature (C): "); Serial.print(y);
+  Serial.print("\t\tHumidity (%): "); Serial.println(x);
+  
 
-  #ifdef GGH
-//reading and storing GGH humidity value
-  GGH_H = SD.open(GGH_GRO_Humidity,FILE_WRITE);
-  GGH_H.println(" %d ",sensor.readHumidity());
-  GGH_H.close();
-
-//reading and storing GGH temp value
-  GGH_T = SD.open(GGH_GRO_Temp,FILE_WRITE);
-  GGH_T.println(" %d ",sensor.readTemp());
-  GGH_T.clos();
-
-  #endif
+  GGH_H.println(x);
+  GGH_H.flush();
+  GGH_T.println(y);
+  GGH_T.flush();
 
   delay(1000);
+
+
+
+ 
+  
+
 }
