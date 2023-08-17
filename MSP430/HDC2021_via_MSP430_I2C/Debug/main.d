44ab6faab22c435d3ac8c6a727f66a7e2fa0a4ad
Tim Kraemer <timkraemer@eduroam-169-233-247-110.ucsc.edu>
2023-08-17 12:12:11 -0700

    Configuration functions for HDC2080 done

--- MSP430/HDC2021_via_MSP430_I2C/Debug/main.d
+++ MSP430/HDC2021_via_MSP430_I2C/Debug/main.d
@@ -1,37 +1,37 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/ti/ccs1240/ccs/ccs_base/msp430/include/msp430.h
-main.obj: C:/ti/ccs1240/ccs/ccs_base/msp430/include/msp430fr5969.h
-main.obj: C:/ti/ccs1240/ccs/ccs_base/msp430/include/in430.h
-main.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
-main.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
-main.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdlib.h
-main.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
-main.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
-main.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+main.obj: /Applications/ti/ccs1240/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /Applications/ti/ccs1240/ccs/ccs_base/msp430/include/msp430fr5969.h
+main.obj: /Applications/ti/ccs1240/ccs/ccs_base/msp430/include/in430.h
+main.obj: /Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: /Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: /Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdlib.h
+main.obj: /Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+main.obj: /Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+main.obj: /Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
 main.obj: ../MSP430_I2C.h
 main.obj: ../HDC2021.h
 
 ../main.c:
 
-C:/ti/ccs1240/ccs/ccs_base/msp430/include/msp430.h:
+/Applications/ti/ccs1240/ccs/ccs_base/msp430/include/msp430.h:
 
-C:/ti/ccs1240/ccs/ccs_base/msp430/include/msp430fr5969.h:
+/Applications/ti/ccs1240/ccs/ccs_base/msp430/include/msp430fr5969.h:
 
-C:/ti/ccs1240/ccs/ccs_base/msp430/include/in430.h:
+/Applications/ti/ccs1240/ccs/ccs_base/msp430/include/in430.h:
 
-C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+/Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
 
-C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+/Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
 
-C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdlib.h:
+/Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdlib.h:
 
-C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+/Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
 
-C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+/Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
 
-C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+/Applications/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
 
 ../MSP430_I2C.h:
 
--- MSP430/HDC2021_via_MSP430_I2C/HDC2021.c
+++ MSP430/HDC2021_via_MSP430_I2C/HDC2021.c
@@ -6,6 +6,7 @@
  */
 
 #include <stdlib.h>
+#include <stdint.h>
 #include "HDC2021.h"
 #include "MSP430_I2C.h"
 
@@ -16,3 +17,91 @@
 #define HUMIDITY_HI_REG 0x03
 #define CONFIG_REG 0x0E
 #define MEAS_CONFIG_REG 0x0F
+
+float Sensor_ReadTemp(void) {
+	
+}
+
+float Sensor_ReadHumidity() {
+
+}
+
+
+void Sensor_TriggerMeasurement(void) {
+
+}
+
+void Sensor_SetMeasurementRate(int Rate) {
+	uint8_t configContents[1] = {0};
+	//Read register CONFIG_REG to configContents[0]
+	switch(Rate) {
+		case MANUAL:
+			configContents[0] = configContents[0] & 0x8F;
+			break;
+		case TWO_MINS:
+			configContents[0] = configContents[0] & 0x9F;
+			configContents[0] = configContents[0] | 0x10;
+			break;
+		case ONE_MINS:
+			configContents[0] = configContents[0] & 0xAF;
+			configContents[0] = configContents[0] | 0x20;
+			break;
+		case TEN_SECONDS:
+			configContents[0] = configContents[0] & 0xBF;
+			configContents[0] = configContents[0] | 0x30;
+			break;
+		case FIVE_SECONDS:
+			configContents[0] = configContents[0] & 0xCF;
+			configContents[0] = configContents[0] | 0x40;
+			break;
+		default:
+			configContents[0] = configContents[0] & 0x8F;
+	}
+	//Write configContents[0] to register CONFIG_REG
+}
+
+void Sensor_MeasurementMode(int Mode) {
+	uint8_t configContents[1] = {0};
+	//Read register MEAS_CONFIG_REG to configContents[0]
+	switch(Mode) {
+		case TEMP_AND_HUMID:
+			configContents[0] = configContents[0] & 0xF9;
+			break;
+		case TEMP_ONLY:
+			configContents[0] = configContents[0] & 0xFC;
+			configContents[0] = configContents[0] | 0x02;
+			break;
+		case HUMID_ONLY:
+			configContents[0] = configContents[0] & 0xFD;
+			configContents[0] = configContents[0] | 0x04;
+			break;
+		default:
+			configContents[0] = configContents[0] & 0xF9;
+	}
+	//Write configContents[0] to register 0x0F
+}
+
+void Sensor_SetHumidityResolution(int Resolution) {
+    uint8_t configContents[1] = {0};
+    //Read register MEAS_CONFIG_REG to configContents[0]
+    switch(Resolution) {
+    case FOURTEEN_BIT:
+        configContents[0] = configContents[0] & 0x3F;
+        break;
+    case ELEVEN_BIT:
+        configContents[0] = configContents[0] & 0x7F;
+        configContents[0] = configContents[0] | 0x40;
+        break;
+    case NINE_BIT:
+        configContents[0] = configContents[0] & 0xB7;
+        configContents[0] = configContents[0] | 0x80;
+        break;
+    default:
+        configContents[0] = configContents[0] & 0x3F;
+    }
+    //Write configContents[0] to MEAS_CONFIG_REG
+}
+
+void Sensor_SetTempResolution(int Resolution) {
+
+}
