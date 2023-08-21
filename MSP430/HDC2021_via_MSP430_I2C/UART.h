54bbac801b73affa5014a7438067c4d4c4d127e2
MatthewKalt <mkaltman@ucsc.edu>
2023-08-21 12:48:46 -0700

    Added UART lib skeleton and uart example to correct folder

---
+++ MSP430/HDC2021_via_MSP430_I2C/UART.h
@@ -0,0 +1,29 @@
+/*
+ * UART.h
+ * Header file for implementation of UART communication with the MSP430FR5969
+ * Adapted from TI's msp430fr59xx_euscia_0uart2 example
+ * Written in Code Composer Studio 12.4
+ * Created on: Aug 21, 2023
+ * Author: Tim Kraemer
+ * Last Modified: 8/21/23
+ */
+
+#ifndef MSP430_UART_H_
+#define MSP430_UART_H_
+
+#include <stdint.h>
+
+
+/*
+ * Enables UART interrupts and sets initial timers
+ */
+void initUART(void);
+
+/*
+ * Raises TX interrupt flag and transmits data byte by byte through UART TX buffer
+ * Once all bytes are passed through, lowers TX interrupt flag
+ * data: char pointer (string) of data to be passed
+ */
+void TXTransmit(char* data);
+
+#endif
