3c2871fbdcf1fa0ac44ccea134f273517b45e5a5
Tim Kraemer <tim022402@gmail.com>
2023-08-21 12:30:52 -0700

    UART example added to repo

---
+++ MSP430/UART_Example/UART.c
@@ -0,0 +1,23 @@
+/*
+ * UART.c
+ * Header file for implementation of UART communication with the MSP430FR5969
+ * Adapted from TI's msp430fr59xx_euscia_0uart2 example
+ * Written in Code Composer Studio 12.4
+ * Created on: Aug 21, 2023
+ * Author: Tim Kraemer
+ * Last Modified: 8/21/23
+ */
+
+#include <stdlib.h>
+#include <msp430.h>
+#include <stdio.h>
+
+#include "UART.h"
+
+void initUART() {
+
+}
+
+void TXTransmit(char* data) {
+
+}
