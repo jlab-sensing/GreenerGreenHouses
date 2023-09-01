26bc79710b103142271c99b3ef2933da41b684f2
Tim Kraemer <tim022402@gmail.com>
2023-09-01 15:21:41 -0700

    SPI UART test formatting and code comments, removed UART_example and LaunchPad_trx_demo directories due to old, outdated, and non functioning code

--- MSP430/Final_SPI_Attempt/main.c
+++ MSP430/Final_SPI_Attempt/main.c
@@ -1,3 +1,5 @@
+
+//Essential includes
 #include <msp430.h> 
 #include "hal_spi_rf_trxeb.h"
 #include "cc112x_spi.h"
@@ -5,7 +7,6 @@
 #include "stdint.h"
 #include "smartRF1125.h"
 #include <stdio.h>
-//#include <driverlib/MSP430FR5xx_6xx/cs.h>
 #include <eusci_a_uart.h>
 #include <gpio.h>
 #include <hw_memmap.h>
@@ -14,16 +15,23 @@
 //#include <driverlib/MSP430FR5xx_6xx/eusci_b_spi.h>
 //#include <pmm.h>
 //#include <wdt_a.h>
-/**
- * main.c
- */
+//#include <driverlib/MSP430FR5xx_6xx/cs.h>
 
+/*
+* transmits character over UART TX transmission
+* param: c, char type, single character to transmit
+*/
 void putcha(char c){
 
  EUSCI_A_UART_transmitData(EUSCI_A0_BASE,c);
 
 }
 
+/*
+* transmits whole string over UART TX transmission, one character at a time using
+* putcha
+* param: *s, char pointer to string to transmit
+*/
 void putstring(char *s){
     int i= 0;
     int length = ((strlen(s)) - 1);
@@ -34,14 +42,18 @@ void putstring(char *s){
     }
 }
 
-uint8_t DeviceID[8] = {0};
-uint8_t DevAdd[1] = {0xA};
-char Jack[512] = {0};
+
+uint8_t DeviceID[8] = {0};		
+uint8_t DevAdd[1] = {0xA};		//Custom slave address for BOOSTXL CC1125 LoRa pack
+char Jack[512] = {0};			//Test char array for UART transmission
+
+//main
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	PM5CTL0 &= ~LOCKLPM5;
+	
 	/* Select Port J
 	* Set Pin 4, 5 to input Primary Module Function, LFXT.
 	*/
@@ -68,7 +80,6 @@ int main(void)
 	// Configure UART pins
 	//Set P2.0 and P2.1 as Secondary Module Function Input.
 	/*
-
 	* Select Port 2d
 	* Set Pin 0, 1 to input Secondary Module Function, (UCA0TXD/UCA0SIMO, UCA0RXD/UCA0SOMI).
 	*/
@@ -78,6 +89,7 @@ int main(void)
 	GPIO_SECONDARY_MODULE_FUNCTION
 	);
 
+	//UART parameter object, setttings set for UART communication
 	EUSCI_A_UART_initParam param;
 	param.selectClockSource = EUSCI_A_UART_CLOCKSOURCE_SMCLK;
 	param.clockPrescalar = 0x6;
@@ -90,29 +102,41 @@ int main(void)
 	param.overSampling = 0x1;
 
 
+	//UART initialization failure check
 	if(STATUS_FAIL ==EUSCI_A_UART_init(EUSCI_A0_BASE,&param)){
 	  return 0;
 	}
 
+	//enable UART block
 	EUSCI_A_UART_enable(EUSCI_A0_BASE);
-	 EUSCI_A_UART_clearInterrupt(EUSCI_A0_BASE,
-	    EUSCI_A_UART_RECEIVE_INTERRUPT);
 
-	  // Enable USCI_A0 RX interrupt
-	  EUSCI_A_UART_enableInterrupt(EUSCI_A0_BASE,
-	    EUSCI_A_UART_RECEIVE_INTERRUPT);                     // Enable interrupt
+	//reset interupt
+	EUSCI_A_UART_clearInterrupt(EUSCI_A0_BASE,
+		EUSCI_A_UART_RECEIVE_INTERRUPT);
 
-	  __enable_interrupt();
+	// Enable USCI_A0 RX interrupt
+	EUSCI_A_UART_enableInterrupt(EUSCI_A0_BASE,
+		EUSCI_A_UART_RECEIVE_INTERRUPT);
 
+	__enable_interrupt();
+
+	//initialize SPI block, set 4 as prescaller for SCLK
 	trxRfSpiInterfaceInit(4);
+
+	//write custom slave address to device address register, length 1 (8bits)
 	cc112xSpiWriteReg(CC112X_DEV_ADDR,DevAdd, 1);
 
+	//read from device address register to verify corrrect slave address set
 	cc112xSpiReadReg(CC112X_DEV_ADDR,DeviceID, 1);
 	uint16_t i = 0;
+
+	//print BOOSTXL CC1125 register settings to serial terminal for correct settings verification
     for(i = 0; i < (sizeof(preferredSettings)/sizeof(registerSetting_t)); i++) {
      sprintf(Jack,"%d\t0x%02X\t0x%02X\n", i, preferredSettings[i].data, preferredSettings[i].addr);
      putstring(Jack);
     }
+
+	//infinite while loop program trap
 	int j=0;
 
 	while(1){
