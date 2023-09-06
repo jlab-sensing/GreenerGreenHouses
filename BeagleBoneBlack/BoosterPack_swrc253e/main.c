8335a0f6f12b2ba8593d80bf7e9551e06bc7b4a7
Jack Lin <jlin143@ucsc.edu>
2023-09-06 16:21:00 -0700

    bbb rx, only 1 byte rx?

--- BeagleBoneBlack/BoosterPack_swrc253e/main.c
+++ BeagleBoneBlack/BoosterPack_swrc253e/main.c
@@ -15,16 +15,24 @@
 // #define uint8_t u_int8_t
 // #define SPI_BUFFER_SIZE 4
 
+// #define LIST_ALL_REGISTERS
+// #define PACKET_PARSER
+#define VARIABLE_LENGTH_PACKET
+// #define FIXED_PACKET_LENGTH 6
+
 #define SPI_DEV_BUS0_CS0 "/dev/spidev0.0"
 
 #define CC1125_REG_READ_nWRITE 0x80
 #define CC1125_REG_BURST 0x40
 
+#define RX_FIFO_ERROR 0x11
+
 // Global
 int spiFileDesc = 0;
 
 static void manualCalibration(void);
-static void registerConfig(void);
+static void registerConfig(const registerSetting_t *reg, int numRegisters);
+static void packetParser(uint8 *packet, uint8 length);
 
 int main(int argc, char *argv[]){
     
@@ -51,28 +59,39 @@ int main(int argc, char *argv[]){
         exit(1);
     }
     
-    
-    registerConfig();
+    printf("sizeof(preferredSettings) = %d\n", sizeof(preferredSettings));
+    printf("sizeof(&preferredSettings) = %d\n", sizeof(&preferredSettings));
+    printf("sizeof(registerSetting_t) = %d\n", sizeof(registerSetting_t));
+    registerConfig(preferredSettings, sizeof(preferredSettings)/sizeof(registerSetting_t));
     
     manualCalibration();
-    trxSpiCmdStrobe(CC112X_SRX);
     
     rfStatus_t status = 0;
     uint8 readByte;
     printf("\n\n\n");
+    
+#ifdef LIST_ALL_REGISTERS
     // Read ALL registers from radio
     printf("Reading ALL registers\n");
+    printf("addr\tvalue\tstatus\n");
     for(uint16 i = 0; i < (sizeof(allSettings)/sizeof(registerSetting_t)); i++) {
         status = cc112xSpiReadReg(allSettings[i].addr, &readByte, 1);
-        printf("[0x%04X]\t0x%02X\t0x%02X\n", allSettings[i].addr, readByte, status);
+        printf("0x%04X\t0x%02X\t0x%02X\n", allSettings[i].addr, readByte, status);
     }
-    printf("\n\n\n");
+    printf("Done reading ALL registers\n\n\n");
+#endif
     
-    uint16 address = 0x00;
-    uint8 rxData[SPI_BUFFER_SIZE];
-    for (int i = 0; i < sizeof(rxData); i++) rxData[i] = 0;
+    // uint16 address = 0x00;
+    uint8 rxBuffer[SPI_BUFFER_SIZE] = {0};
+    // for (int i = 0; i < sizeof(rxData); i++) rxData[i] = 0;
+    uint8 rxBytes;
+    uint8 marcState;
+    
+    printf("Set RX mode\n");
+    trxSpiCmdStrobe(CC112X_SRX);
     
     while(1){
+        /*
         // SPI_transfer(spiFileDesc, txBuffer, rxBuffer, SPI_BUFFER_SIZE);
         // sleep(1);
         // printf("\nrxBuffer: ");
@@ -92,21 +111,49 @@ int main(int argc, char *argv[]){
         
         // printf("[0x%02X] status = 0x%02X\trxData = 0x%02X\n", address, status, rxData[0]);
         
-        status = cc112xGetRxStatus();
-        printf("GetRxStatus status = 0x%02X\n", status);
-        uint8_t rxStatus_chip;
-        uint8_t rxStatus_state;
-        uint8_t rxStatus_rxfifobytes;
-        rxStatus_chip = status >> 7
-        rxStatus_state;
-        rxStatus_rxfifobytes;
-        printf("\tCHIP_RDY | STATE[2:0] | FIFO_BYTES_AVAILABLE (available bytes in the RX FIFO\n");
-        status = cc112xSpiReadRxFifo(rxData, length);
-        printf("ReadRxFifo status = 0x%02X\n", status);
-        sleep(1);
+        // status = cc112xGetRxStatus();
+        // printf("\n\nGetRxStatus status = 0x%02X\n", status);
+        // uint8_t rxStatus_chip;
+        // uint8_t rxStatus_state;
+        // uint8_t rxStatus_rxfifobytes;
+        // rxStatus_chip = status >> 7
+        // rxStatus_state;
+        // rxStatus_rxfifobytes;
+        // printf("\tCHIP_RDY | STATE[2:0] | FIFO_BYTES_AVAILABLE (available bytes in the RX FIFO\n");
+        */
+        
+        status = cc112xSpiReadReg(CC112X_NUM_RXBYTES, &rxBytes, 1);
+        
+        if (rxBytes != 0){
+            printf("RX FIFO %d bytes, status = 0x%02X\n", rxBytes, status);
+            // Check MARCSTATE for RX FIFO error
+            // Flush RX FIFO if error
+            // Else read RX FIFO
+            status = cc112xSpiReadReg(CC112X_MARCSTATE, &marcState, 1);
+
+            // Mask out MARCSTATE bits and check if we have a RX FIFO error
+            if((marcState & 0x1F) == RX_FIFO_ERROR) {
+                // Flush RX FIFO
+                trxSpiCmdStrobe(CC112X_SFRX);
+            } else {
+                status = cc112xSpiReadRxFifo(rxBuffer, rxBytes);
+                // status = cc112xSpiReadRxFifo(rxBuffer, SPI_BUFFER_SIZE);
+                printf("FIFO (0x%02X) {hex} : ", status);
+                for (int i = 0; i < rxBytes; i++){
+                    printf("%02X ", rxBuffer[i]);
+                }
+                printf("\n");
+#ifdef PACKET_PARSER
+                packetParser(rxBuffer, rxBytes);
+#endif
+            }
+            trxSpiCmdStrobe(CC112X_SRX);
+            usleep(10);
+        }
+        // trxSpiCmdStrobe(CC112X_SRX);
     }
 
-    printf("Closing \"/dev/spidev0.0\".\n");
+    printf("Closing %s.\n", SPI_DEV_BUS0_CS0);
     // fclose(spiFileDesc);
     return 0;
 }
@@ -121,18 +168,24 @@ int main(int argc, char *argv[]){
 *
 *   @return     none
 */
-static void registerConfig(void) {
+static void registerConfig(const registerSetting_t *reg, int numRegisters) {
 
     uint8 writeByte;
     uint8 readByte;
     rfStatus_t status;
     
-    for(uint16 i = 0; i < (sizeof(preferredSettings)/sizeof(registerSetting_t)); i++) {
-        printf("%d\t0x%02X\t0x%02X\n", i, preferredSettings[i].data, preferredSettings[i].addr);
+    printf("sizeof(reg) = %d\n", sizeof(reg));
+    printf("sizeof(&reg) = %d\n", sizeof(&reg));
+    
+    printf("Registers to write\n");
+    printf("#\taddr\tvalue\n");
+    for(uint16 i = 0; i < numRegisters; i++) {
+        printf("%d\t0x%02X\t0x%02X\n", i, reg[i].addr, reg[i].data);
     }
+    printf("Done listing registers to write\n");
 
     // Reset radio
-    printf("Resetting radio.\n");
+    printf("Resetting radio\n");
     status = trxSpiCmdStrobe(CC112X_SRES);
     printf("Status byte: 0x%02X\n", status);
     
@@ -140,41 +193,44 @@ static void registerConfig(void) {
     sleep(1);
     
     // Read registers from radio
-    printf("Reading\n");
-    for(uint16 i = 0; i < (sizeof(preferredSettings)/sizeof(registerSetting_t)); i++) {
-        printf("i = %d\t", i);
-        printf("expect = 0x%02X\t", preferredSettings[i].data);
-        status = cc112xSpiReadReg(preferredSettings[i].addr, &readByte, 1);
-        printf("value = 0x%02X\t", readByte);
+    printf("Reading registers that will be written to\n");
+    printf("#\taddr\texpect\tvalue\tcompare\n");
+    for(uint16 i = 0; i < numRegisters; i++) {
+        printf("%d\t0x%02X\t0x%02X", i, reg[i].addr, reg[i].data);
+        status = cc112xSpiReadReg(reg[i].addr, &readByte, 1);
         
-        if (readByte == preferredSettings[i].data){
+        if (readByte == reg[i].data){
+            printf(" == 0x%02X\t", readByte);
             printf("OK\n");
         }else{
+            printf(" != 0x%02X\t", readByte);
             printf("UNEXPECTED REGISTER VALUE\n");
         }
     }
 
     // Write registers to radio
-    printf("Writing\n");
-    for(uint16 i = 0; i < (sizeof(preferredSettings)/sizeof(registerSetting_t)); i++) {
-        printf("i = %d\t", i);
-        writeByte = preferredSettings[i].data;
-        printf("writeByte = 0x%02X\t", writeByte);
-        status = cc112xSpiWriteReg(preferredSettings[i].addr, &writeByte, 1);
-        printf("status = 0x%02X\n", status);
+    printf("Writing registers based on reg\n");
+    printf("#\taddr\t    write\n");
+    for(uint16 i = 0; i < numRegisters; i++) {
+        printf("%d\t", i);
+        writeByte = reg[i].data;
+        printf("0x%02X\t<-- 0x%02X\n", reg[i].addr, writeByte);
+        status = cc112xSpiWriteReg(reg[i].addr, &writeByte, 1);
+        // printf("status = 0x%02X\n", status);
     }
     
     // Read registers from radio
-    printf("Reading\n");
-    for(uint16 i = 0; i < (sizeof(preferredSettings)/sizeof(registerSetting_t)); i++) {
-        printf("i = %d\t", i);
-        printf("expect = 0x%02X\t", preferredSettings[i].data);
-        status = cc112xSpiReadReg(preferredSettings[i].addr, &readByte, 1);
-        printf("value = 0x%02X\t", readByte);
+    printf("Reading registers that were written to\n");
+    printf("#\taddr\texpect\tvalue\tcompare\n");
+    for(uint16 i = 0; i < numRegisters; i++) {
+        printf("%d\t0x%02X\t0x%02X", i, reg[i].addr, reg[i].data);
+        status = cc112xSpiReadReg(reg[i].addr, &readByte, 1);
         
-        if (readByte == preferredSettings[i].data){
+        if (readByte == reg[i].data){
+            printf(" == 0x%02X\t", readByte);
             printf("OK\n");
         }else{
+            printf(" != 0x%02X\t", readByte);
             printf("UNEXPECTED REGISTER VALUE\n");
         }
     }
@@ -272,5 +328,55 @@ static void manualCalibration(void) {
         cc112xSpiWriteReg(CC112X_FS_VCO4, &writeByte, 1);
         writeByte = calResults_for_vcdac_start_mid[FS_CHP_INDEX];
         cc112xSpiWriteReg(CC112X_FS_CHP, &writeByte, 1);
+    }
+}
+
+static void packetParser(uint8 *packet, uint8 length)
+{
+    /*  Packet Structure for temp/humidity
+        | Device ID (4 bits) | Temperature (14 bits) | Humidity (14 bits) | CRC16 (16 bits) |
+        
+        0b0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000
+          [ID] [  Temperature  ][    Humidity   ] [      CRC16      ]
+        
+        ID (store as uint8_t)
+        xxxx ABCD
+        0: upp 4    ABCD xxxx
+        
+        Temperature (store as uint16_t)
+        xxAB CDEF GHIJ KLMN
+        0: low 4    xxxx ABCD
+        1: all 8    EFGH IJKL
+        2: upp 2    MNxx xxxx
+        
+        Humidity (store as uint16_t)
+        xxAB CDEF GHIJ KLMN
+        2: low 6    xxAB CDEF
+        3: all 8    GHIJ KLMN
+        
+        CRC16 (store as uint16_t)
+        ABCD EFGH IJKL MNOP
+        4: all 8    ABCD EFGH
+        5: all 8    IJKL MNOP
+    */
+    
+    // To do: Return struct which breaks out the received raw packet into its constituent segments
+    // Consider: What about multi-packet messages? Out of scope?
+    
+    if (length == 6){
+        uint8   id = (packet[0] & 0xF0) >> 4;
+        uint16  temperature =   ((packet[0] & 0x0F) << 10) | \
+                                ((packet[1] & 0xFF) << 2) | \
+                                ((packet[2] & 0xC0) >> 6);
+        uint16  humidity =  ((packet[2] & 0x3F) << 8) | \
+                            ((packet[3] & 0xFF));
+        uint16  crc = (packet[4] << 8) | (packet[5]);
+        
+        printf("\tID:\t0x%01X (%d)\n", id, id);
+        printf("\tTemp:\t0x%04X (%d)\n", temperature, temperature);
+        printf("\tHumi:\t0x%04X (%d)\n", humidity, humidity);
+        printf("\tCRC16:\t0x%04X\n", crc);
+    }else{
+        printf("Unrecognized packet format\n");
     }
 }
