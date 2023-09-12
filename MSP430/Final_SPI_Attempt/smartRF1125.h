dd0905d243fd29fe3e8bc66775c52af492a63707
Tim Kraemer <tim022402@gmail.com>
2023-09-12 13:01:50 -0700

    more code formatting and comments

--- MSP430/Final_SPI_Attempt/smartRF1125.h
+++ MSP430/Final_SPI_Attempt/smartRF1125.h
@@ -149,10 +149,8 @@ static const registerSetting_t ETSI_CAT1_869_S1_PacketTxRx[] = {
 {CC112X_FIFO_CFG,          0x00},
 {CC112X_SETTLING_CFG,      0x03},
 {CC112X_FS_CFG,            0x12},
-//0x20 variable length mode, 0x00 for fixed length mode
-{CC112X_PKT_CFG0,          0x00},
-//set to 0x06, fixed packet length of "Test #"
-{CC112X_PKT_LEN,           0x04},
+{CC112X_PKT_CFG0,          0x00},           //0x20 variable length mode, 0x00 for fixed length mode
+{CC112X_PKT_LEN,           0x04},           //set to 0x04, fixed packet size
 {CC112X_IF_MIX_CFG,        0x00},
 {CC112X_FREQOFF_CFG,       0x22},
 {CC112X_FREQ2,             0x56},
