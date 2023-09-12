c751d2d8ee104d3a5637fa2e1f8526c773e66cb5
Jack Lin <jlin143@ucsc.edu>
2023-09-12 15:38:07 -0700

    Updated packet parser to calculate temperature and humidity based on the upper 14 bits

--- BeagleBoneBlack/rxlogger/main.c
+++ BeagleBoneBlack/rxlogger/main.c
@@ -13,13 +13,18 @@
 #include "cc112x_easy_link_reg_config.h"
 
 // #define LIST_ALL_REGISTERS
-// #define PACKET_PARSER
-#define SKIP_LORA
+#define TEST_PACKET_PARSER
+#define PARSE_PACKET_CAST_32
+// #define SKIP_LORA
+// #define SKIP_MODBUS
 
-#define RX_BUFFER_SIZE 16
+#define MAX_PACKET_LENGTH 16
 
 #define SPI_DEV_BUS0_CS0 "/dev/spidev0.0"
 
+#define MARCSTATE_NOT_USED_MASK 0x80
+#define MARC_2PIN_STATE_MASK 0x60
+#define MARC_STATE_MASK 0x1F
 #define RX_FIFO_ERROR 0x11
 
 #define MODBUS_SLAVE_ID 0xAB
@@ -35,6 +40,21 @@
 #define RTU_DATA_BITS   8
 #define RTU_STOP_BITS   1
 
+typedef struct {
+    int deviceID;
+    uint16_t temperature_raw;
+    float temperature_scaled;
+    uint16_t humidity_raw;
+    float humidity_scaled;
+} packetStruct_t;
+
+typedef enum {
+    RH_T_14_BIT,
+    RH_T_11_BIT,
+    RH_T_9_BIT,
+    UNKNOWN
+} packetType_t;
+
 // Global
 static FILE* fp = NULL;
 static modbus_t *mb = NULL;
@@ -42,7 +62,7 @@ static modbus_mapping_t *map;
 
 static void manualCalibration(void);
 static void registerConfig(const registerSetting_t *reg, int numRegisters);
-static void packetParser(uint8 *packet, uint8 length);
+static packetType_t packetParser(uint8 *packet, uint8 length, packetStruct_t *returnPacket);
 void intHandler(int sig);
 modbus_t * modbus_setup(const char *device, int baud, char parity, int data_bit, int stop_bit);
 void print_modbus_mapping(void);
@@ -121,14 +141,38 @@ int modbus_init(unsigned int bits, unsigned int ibits, unsigned int iregs, unsig
 }
 
 int main(int argc, char *argv[]){
-    rfStatus_t status = 0;
-    uint8 rxBuffer[RX_BUFFER_SIZE] = {0};
-    uint8_t rxBytes;
-    uint8 marcState;
-    uint8 lastMarcState;
-    int req_length;
-    uint8_t req[MODBUS_MAX_ADU_LENGTH];
+#ifdef TEST_PACKET_PARSER
+    printf("argc = %d\n", argc);
+    for (int i = 0; i < argc; i++){
+        printf("%s\n", argv[i]);
+    }
+    if (argc < 2){
+        printf("TEST_PACKET_PARSER usage: <program> <1 byte hex> <1 byte hex> {max 16 bytes}\nExample: ./out 30 E5 6F 1A");
+        return -1;
+    }
     
+    // All arguments (after the name of the program) are interpreted as the data packet
+    uint8 testBuffer[MAX_PACKET_LENGTH] = {0};
+    for (int i = 1; i < argc; i++){
+        testBuffer[i - 1] = strtoul(argv[i], NULL, 16);
+        printf("testBuffer[%d] = 0x%02X\n", i - 1, testBuffer[i - 1]);
+    }
+    
+    packetStruct_t parsedPacket;
+    printf("Calling packetParser\n");
+    switch (packetParser(testBuffer, argc - 1, &parsedPacket)){
+        case RH_T_14_BIT:
+        case RH_T_11_BIT:
+        case RH_T_9_BIT:
+            printf("%d: temp 0x%X %f, hum 0x%X %f\n", parsedPacket.deviceID, parsedPacket.temperature_raw, parsedPacket.temperature_scaled, parsedPacket.humidity_raw, parsedPacket.humidity_scaled);
+            break;
+        case UNKNOWN:
+            break;
+        default:
+            break;
+    }
+    return 0;
+#endif
     printf("Program start.\n");
     
     signal(SIGINT, intHandler);
@@ -148,14 +192,22 @@ int main(int argc, char *argv[]){
     trxSpiCmdStrobe(CC112X_SRX);
     
     while(1){
-        // LoRa
 #ifndef SKIP_LORA
+        // LoRa
+        
+        rfStatus_t status;
+        uint8 rxBuffer[MAX_PACKET_LENGTH] = {0};
+        uint8_t rxBytes;
+        uint8 marcState;
+        uint8 lastMarcState;
+        packetStruct_t parsedPacket;
+
         status = cc112xSpiReadReg(CC112X_MARCSTATE, &marcState, 1);
         if (marcState == lastMarcState){
             continue;
         }
         
-        if((marcState & 0x1F) == RX_FIFO_ERROR) {
+        if((marcState & MARC_STATE_MASK) == RX_FIFO_ERROR) {
             printf("\tMARCSTATE RX_FIFO_ERROR, flushing\n");
             trxSpiCmdStrobe(CC112X_SFRX);
         }
@@ -170,18 +222,33 @@ int main(int argc, char *argv[]){
             }
             printf("\n");
             
+            switch (packetParser(rxBuffer, rxBytes - 2, &parsedPacket)){
+                case RH_T_14_BIT:
+                case RH_T_11_BIT:
+                case RH_T_9_BIT:
+                    break;
+                case UNKNOWN:
+                    break;
+                default:
+                    break;
+            }
+            
             printf("writing\n");
             fwrite(rxBuffer, rxBytes, sizeof(rxBuffer[0]), fp);
         }
         lastMarcState = marcState;
         trxSpiCmdStrobe(CC112X_SRX);
 #endif
+#ifndef SKIP_MODBUS
         // Modbus
+        
+        int requestLength, responseLength;
+        uint8_t req[MODBUS_MAX_ADU_LENGTH];
         
         print_modbus_mapping();
         printf("waiting on modbus_receive\n");
-        req_length = modbus_receive(mb, req);
-        switch(req_length){
+        requestLength = modbus_receive(mb, req);
+        switch(requestLength){
             case 0:
                 printf("Ignore request (other slave address)\n");
                 break;
@@ -189,16 +256,17 @@ int main(int argc, char *argv[]){
                 printf("Error receiving\n");
                 break;
             default:
-                printf("Handling request (length %d)\n", req_length);
+                printf("Handling request (length %d)\n", requestLength);
                 sleep(1);
-                status = modbus_reply(mb, req, req_length, map);
-                if (status == -1){
+                responseLength = modbus_reply(mb, req, requestLength, map) ;
+                if (responseLength == -1){
                     printf("Could not reply\n");
                 }else{
-                    printf("Sent reply (length %d)\n", status);
+                    printf("Sent reply (length %d)\n", responseLength);
                 }
                 break;
         }
+#endif
     }
         
     printf("Closing %s.\n", SPI_DEV_BUS0_CS0);
@@ -400,53 +468,111 @@ static void manualCalibration(void) {
     }
 }
 
-static void packetParser(uint8 *packet, uint8 length)
+// Do not include RSSI (2 bytes)
+// returnPacket will be populated based on the packet type
+// packetType describes which fields in returnPacket are relevant
+static packetType_t packetParser(uint8 *packet, uint8 length, packetStruct_t *returnPacket)
 {
     /*  Packet Structure for temp/humidity
         | Device ID (4 bits) | Temperature (14 bits) | Humidity (14 bits) | CRC16 (16 bits) |
         
+          [   0   ] [   1   ] [   2   ] [   3   ] [   4   ] [   5   ]
         0b0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000
-          [ID] [  Temperature  ][    Humidity   ] [      CRC16      ]
+          [ID] [  Temperature  ][    Humidity   ] [      RSSI?      ]
         
         ID (store as uint8_t)
         xxxx ABCD
         0: upp 4    ABCD xxxx
         
         Temperature (store as uint16_t)
-        xxAB CDEF GHIJ KLMN
+        ABCD EFGH IJKL MNxx
         0: low 4    xxxx ABCD
         1: all 8    EFGH IJKL
         2: upp 2    MNxx xxxx
         
         Humidity (store as uint16_t)
-        xxAB CDEF GHIJ KLMN
+        ABCD EFGH IJKL MNxx
         2: low 6    xxAB CDEF
         3: all 8    GHIJ KLMN
-        
-        CRC16 (store as uint16_t)
-        ABCD EFGH IJKL MNOP
-        4: all 8    ABCD EFGH
-        5: all 8    IJKL MNOP
     */
     
     // To do: Return struct which breaks out the received raw packet into its constituent segments
     // Consider: What about multi-packet messages? Out of scope?
     
-    if (length == 6){
-        uint8   id = (packet[0] & 0xF0) >> 4;
-        uint16  temperature =   ((packet[0] & 0x0F) << 10) | \
-                                ((packet[1] & 0xFF) << 2) | \
-                                ((packet[2] & 0xC0) >> 6);
-        uint16  humidity =  ((packet[2] & 0x3F) << 8) | \
-                            ((packet[3] & 0xFF));
-        uint16  crc = (packet[4] << 8) | (packet[5]);
+    if (length <= 6){ // Identify packet type based on length
+#ifdef PARSE_PACKET_CAST_32
+        uint32_t bigPacket = (packet[0] << 24) | (packet[1] << 16) | (packet[2] << 8) | (packet[3]);
+        // printf("%X\n", bigPacket);
+        
+        uint8_t  id             = (bigPacket & 0xF0000000) >> 28; // align LSB
+        uint16_t temperature    = (bigPacket & 0x0FFFC000) >> 12; // align MSB
+        uint16_t humidity       = (bigPacket & 0x00003FFF) <<  2; // align MSB
+        
+        // printf("%01X %04X %04X\n", id, temperature, humidity);
+        
+        returnPacket->deviceID = id;
+        returnPacket->temperature_raw = temperature;
+        returnPacket->temperature_scaled = (((float)temperature * 165) / 65536) - 40;
+        returnPacket->humidity_raw = humidity;
+        returnPacket->humidity_scaled = ((float)humidity / 65536) * 100;
+        
+        // printf("temp calcs %X %d %f\n", temperature, temperature, (float)temperature);
+        // printf("\t%f\n", returnPacket->temperature_scaled);
+        // returnPacket->temperature_scaled *= 165.0f;
+        // printf("\t%f\n", returnPacket->temperature_scaled);
+        // returnPacket->temperature_scaled /= 65536.0f;
+        // printf("\t%f\n", returnPacket->temperature_scaled);
+        // returnPacket->temperature_scaled -= 40.0f;
+        // printf("\t%f\n", returnPacket->temperature_scaled);
+        
+        // printf("humidity calcs %X %d %f\n", humidity, humidity, (float)humidity);
+        // returnPacket->humidity_scaled = humidity;
+        // printf("\t%f\n", returnPacket->humidity_scaled);
+        // returnPacket->humidity_scaled /= 65536.0f;
+        // printf("\t%f\n", returnPacket->humidity_scaled);
+        // returnPacket->humidity_scaled *= 100.0f;
+        // printf("\t%f\n", returnPacket->humidity_scaled);
+        
+#else
+        uint8   id =            (packet[0] >> 4) & 0x0F;
+        uint16  temperature =   ((packet[0] << 10) & 0x3C00) | \
+                                ((packet[1] << 2) &  0x03FC) | \
+                                ((packet[2] >> 6) &  0x0003);
+        uint16  humidity =      ((packet[2] << 8) & 0x3F00) | \
+                                (packet[3] & 0x00FF);
+        // uint16  crc =           (packet[4] << 8) | (packet[5]);
+        
+        printf("raw: %02X %02X %02X %02X\n", packet[0], packet[1], packet[2], packet[3]);
+        
+        returnPacket->deviceID = id;
+        returnPacket->temperature_raw = temperature;
+        returnPacket->temperature_scaled = temperature;
+        returnPacket->temperature_scaled = ((returnPacket->temperature_scaled * 165.0f) / 65536.0f) - 40.0f;
+        
+        printf("\ttemp calcs\n");
+        printf("\t\tstart 0x%X %f %A\n", temperature, (float)temperature, (float)temperature);
+        printf("\t\tmult 165.0 %f\n", (float)temperature * 165.0f);
+        printf("\t\tdiv 65536.0f %f\n", ((float)temperature * 165.0f) / 65536.0f);
+        printf("\t\tsub 40.0f %f\n", (((float)temperature * 165.0f) / 65536.0f) - 40.0f);
+        
+        returnPacket->humidity_raw = humidity;
+        returnPacket->humidity_scaled = humidity;
+        returnPacket->humidity_scaled = (returnPacket->humidity_scaled / 65536.0f) * 100.0f;
+        
+        printf("\thumi calcs\n");
+        printf("\t\tstart 0x%X %f\n", humidity, (float)humidity);
+        printf("\t\tdiv 65536.0f %f\n", (float)humidity / 65536.0f);
+        printf("\t\tmult 100.0 %f\n", ((float)humidity / 65536.0f) * 100.0f);
         
         printf("\tID:\t0x%01X (%d)\n", id, id);
-        printf("\tTemp:\t0x%04X (%d)\n", temperature, temperature);
-        printf("\tHumi:\t0x%04X (%d)\n", humidity, humidity);
-        printf("\tCRC16:\t0x%04X\n", crc);
+        printf("\tTemp:\t0x%04X (%d) [%f ; %A]\n", temperature, temperature, returnPacket->temperature_scaled, returnPacket->temperature_scaled);
+        printf("\tHumi:\t0x%04X (%d) [%f ; %A]\n", humidity, humidity, returnPacket->humidity_scaled, returnPacket->humidity_scaled);
+#endif
+        
+        return RH_T_14_BIT;
     }else{
         printf("Unrecognized packet format\n");
+        return UNKNOWN;
     }
 }
 
